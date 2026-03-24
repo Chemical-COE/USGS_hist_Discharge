c863aaeb550db8dd664ac9b90259d22abb657ea0
Chemical-COE <cfirkins@unm.edu>
2026-03-23 22:21:17 -0600

    Create 1_Data.py

---
+++ pages/1_Data.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import os
+import pandas as pd
+import requests
+from dataretrieval import waterdata
+from datetime import date, datetime, timedelta
+from dateutil.relativedelta import relativedelta
+
+st.title("Discharge Data")
+
+if 'us_state' not in st.session_state:
+  st.session_state['us_state'] = None
+
+if 'usgs_key' not in st.session_state:
+    st.session_state['usgs_key'] = None
+    st.warning('Please Enter Your API Key on the APP Page')
+
+else:
+  os.environ['API_USGS_PAT'] = st.session_state['usgs_key']
+
+state = st.text_input("Ex New Mexico")
+try:
+  NM_discharge, _ = waterdata.get_time_series_metadata(
+    state_name=state,
+    parameter_code="00060",
+    skip_geometry=True,
+  )
+
+except:
+  st.warning("Try entering the state again")
