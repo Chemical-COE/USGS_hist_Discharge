21b70ac8ca42c5f81900a85c91fe64243870341d
Chemical-COE <cfirkins@unm.edu>
2026-03-23 21:55:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ if 'usgs_key' not in st.session_state:
 if st.session_state['usgs_key'] == 'no_key':
   st.info("Please Enter your API Key below before Proceeding")
   user_input = st.text_input("Please Type Here")
+  st.link_button("Get your USGS API Key Here", "https://api.waterdata.usgs.gov/signup/")
   
 
   
