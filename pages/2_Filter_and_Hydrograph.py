6c358b91ef4d07cb4a60df04ba1583bccb43a970
Chemical-COE <cfirkins@unm.edu>
2026-08-05 16:46:37 -0600

    Update 2_Filter_and_Hydrograph.py

--- pages/2_Filter_and_Hydrograph.py
+++ pages/2_Filter_and_Hydrograph.py
@@ -61,6 +61,8 @@ fig = px.scatter(
     title='River Hydrograph', # Add A Title. :D
     #markers=True
 )
+fig.update_layout(xaxis_title = 'Day of Year', yaxis_title = 'Discharge (cfs)')
+
 st.plotly_chart(fig)
 
 fig_log = px.scatter(
@@ -72,7 +74,7 @@ fig_log = px.scatter(
     title='River Hydrograph Log Scaled', # Add A Title. :D
     #markers=True
 )
-
+fig.update_layout(xaxis_title = 'Day of Year', yaxis_title = 'Discharge (cfs)')
 fig_log.update_layout(yaxis_type="log")
 st.plotly_chart(fig_log)
 
